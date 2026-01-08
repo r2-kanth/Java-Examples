2897fc88ed496793739d4188eea1754fb9b683b7
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-01-08 18:08:25 +0530

    Logical operators program

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo
+{
+    public static void main(String args [])
+    {
+        int a=10;
+        int b=20;
+        int c=12;
+
+        if ( a<b && b<c)
+        {
+            System.out.println("Hello Boy");
+            System.out.println("How are you now");
+
+        }
+        else 
+        System.out.println("Bye Bro");
+    }
+}
