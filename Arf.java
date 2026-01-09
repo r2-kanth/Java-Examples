6115ce2de373d41ce610164da1dd2d587eb64e3b
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-01-09 16:42:40 +0530

    greatest among three Numbers Program

---
+++ Arf.java
@@ -0,0 +1,15 @@
+public class Arf
+{
+    public static void main(String args[])
+    {
+        int x=2;
+        int y=1;
+        int z=10;
+        if( x>y && x>z)
+        System.out.println(x);
+        else if(y>z)
+        System.out.println(y);
+        else
+        System.out.println(z);
+    }
+}
