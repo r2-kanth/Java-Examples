0a445a2e1dc34116f63ea531cd2313b4790a13f8
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-01-21 21:32:51 +0530

    Switch operation for finding the day of the number

---
+++ Gh.java
@@ -0,0 +1,34 @@
+public class Gh
+{
+    public static void main(String args[])
+    {
+        int n =3 ;
+         
+         switch(n){
+
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thrusday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break; 
+            default:
+             System.out.print("Enter a Valid Number");
+         }
+    }
+}   
