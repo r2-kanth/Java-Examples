9ee970dba289e3f82ba4b3d9024fa3f1faf90475
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-01-21 20:37:16 +0530

    Even or odd

---
+++ Brf.java
@@ -0,0 +1,12 @@
+public class Brf
+{
+    public static void main(String args[])
+    {
+         int a=10;
+        if ( a%2==0 )
+          System.out.println("True");
+        else
+        System.out.println("False");
+    }
+    
+}
