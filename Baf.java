4faca5494292fa1f9e2ce2b77a0a1dbe1de3f826
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-01-21 21:08:03 +0530

    even or odd by Ternary operator

---
+++ Baf.java
@@ -0,0 +1,14 @@
+public class Baf
+{
+    public static void main (String args[])
+    {
+    
+        int n =8;
+        int result =0;
+        
+         result = n%2 ==0 ? 10 : 20 ;
+         System.out.println(result); 
+        // If the number is odd it print 10 or it print 20
+
+    }
+}
