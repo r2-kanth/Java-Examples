2f5e779bf415b638e036e7302d0ae04a3c4e2ee6
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-02-13 12:13:32 +0530

    While loop

---
+++ Fg.java
@@ -0,0 +1,12 @@
+public class Fg
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        int j=1;
+        while(i<=4){
+            System.out.println("Hi");
+            i++;
+        }
+    }
+}
