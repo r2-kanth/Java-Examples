85d3e64811c90949926387685d033a0f9e3dbb99
Ravikanth Chanati <kanthravi8888@gmail.com>
2026-02-13 12:20:33 +0530

    Using while inside the another while

--- Fg.java
+++ Fg.java
@@ -3,9 +3,13 @@ public class Fg
     public static void main(String args[])
     {
         int i=1;
-        int j=1;
         while(i<=4){
             System.out.println("Hi " + i);
+            int j=1;
+            while(j<=3){
+                System.out.println("Ravi " + j);
+                j++;
+            }
             i++;
         }
     }
